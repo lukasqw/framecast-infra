b611675897433e868f9c3a39bcbaddef1e5c5394
lukasqw <lukas.rodrigues.2000@gmail.com>
2026-03-21 22:46:21 -0300

    feat: config

--- terraform/environments/production/aws-auth-configmap.tf
+++ terraform/environments/production/aws-auth-configmap.tf
@@ -2,24 +2,6 @@
 # Este arquivo gerencia o ConfigMap aws-auth do Kubernetes automaticamente
 # detectando o usuário/role atual que está executando o Terraform
 
-# Extrair o nome do usuário/role do ARN
-locals {
-  # ARN completo do caller
-  caller_arn = data.aws_caller_identity.current.arn
-  
-  # Extrair username do ARN
-  # Exemplo: arn:aws:iam::123456789:user/awsstudent -> awsstudent
-  # Exemplo: arn:aws:iam::123456789:role/LabRole -> LabRole
-  caller_username = element(split("/", local.caller_arn), length(split("/", local.caller_arn)) - 1)
-  
-  # Determinar se é user ou role
-  is_user = can(regex(":user/", local.caller_arn))
-  is_role = can(regex(":role/", local.caller_arn))
-  
-  # Account ID
-  account_id = data.aws_caller_identity.current.account_id
-}
-
 # ConfigMap aws-auth
 resource "kubernetes_config_map_v1_data" "aws_auth" {
   # Só criar se o modo de autenticação incluir CONFIG_MAP
