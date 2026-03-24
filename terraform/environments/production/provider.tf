78737dc886a2218b7a9032daf7395fcedb757a78
rafaelqw <rafaeloliveirabispo00@gmail.com>
2026-03-24 00:47:59 -0300

    fix: terraform

--- terraform/environments/production/provider.tf
+++ terraform/environments/production/provider.tf
@@ -5,3 +5,8 @@ provider "aws" {
   # Removed default_tags to avoid conflicts with security group rules
   # Tags are applied directly in each resource instead
 }
+
+provider "datadog" {
+  api_key = var.datadog_api_key
+  app_key = var.datadog_app_key
+}
--- terraform/modules/datadog/main.tf
+++ terraform/modules/datadog/main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    datadog = {
-      source  = "DataDog/datadog"
-      version = "~> 3.0"
-    }
-  }
-}
-
-provider "datadog" {
-  api_key = var.datadog_api_key
-  app_key = var.datadog_app_key
-}
-
 # ------------------------------------------------------------
 # Monitor 1: High API Latency (p95 > 2s)
 # ------------------------------------------------------------
