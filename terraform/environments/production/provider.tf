95461f6fee9e01bb8c09912ce610e62ceed7ed98
lukasqw <lukas.rodrigues.2000@gmail.com>
2026-05-20 01:14:30 -0300

    feat: update Datadog provider configuration for improved readability

--- terraform/environments/production/provider.tf
+++ terraform/environments/production/provider.tf
@@ -7,7 +7,8 @@ provider "aws" {
 }
 
 provider "datadog" {
-  api_key = var.datadog_api_key
-  app_key = var.datadog_app_key
-  api_url = var.datadog_api_url
+  api_key  = var.datadog_api_key
+  app_key  = var.datadog_app_key
+  api_url  = var.datadog_api_url
+  validate = false
 }
