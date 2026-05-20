7c80ee4a3418bf700a8547925a3b3d3ab46eeaa0
lukasqw <lukas.rodrigues.2000@gmail.com>
2026-05-20 19:27:11 -0300

    fix: mover import blocks para root module (producao)

---
+++ terraform/environments/production/imports.tf
@@ -0,0 +1,54 @@
+# Terraform 1.7+ — import blocks são idempotentes:
+# se o recurso já está no state, o bloco é ignorado.
+# Necessário para recuperar o state após um destroy que não deletou
+# os monitors no Datadog (ex.: credenciais ausentes durante o destroy).
+
+import {
+  to = module.datadog[0].datadog_monitor.high_api_latency
+  id = "283667600"
+}
+
+import {
+  to = module.datadog[0].datadog_monitor.service_order_failures
+  id = "283667611"
+}
+
+import {
+  to = module.datadog[0].datadog_monitor.pod_restarts
+  id = "283667608"
+}
+
+import {
+  to = module.datadog[0].datadog_monitor.high_error_rate
+  id = "283667601"
+}
+
+import {
+  to = module.datadog[0].datadog_monitor.high_cpu
+  id = "283667604"
+}
+
+import {
+  to = module.datadog[0].datadog_monitor.high_memory
+  id = "283667602"
+}
+
+import {
+  to = module.datadog[0].datadog_monitor.service_order_processing_errors
+  id = "283667606"
+}
+
+import {
+  to = module.datadog[0].datadog_monitor.service_order_failure_rate
+  id = "283667603"
+}
+
+import {
+  to = module.datadog[0].datadog_monitor.service_order_stuck
+  id = "283667605"
+}
+
+import {
+  to = module.datadog[0].datadog_monitor.service_order_creation_drop
+  id = "283667599"
+}
